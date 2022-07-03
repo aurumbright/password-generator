248db8a4ceb32035c5b310f77211b06b34debd4e
aurumbright <aurum.elean@gmail.com>
2022-07-02 18:25:01 -0700

    updated readme

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,6 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 // write password is the function that posts the password to the page in response to the click
 function writePassword() {
-  // generatePassword is the function that will actually generate the random pw
 
   var inputLength = window.prompt("How long should your password be? Please enter a number between 8 and 128.");
   var passwordLength = parseInt(inputLength);
@@ -17,6 +16,7 @@ function writePassword() {
     return;
   };
 
+  // All input other than yes will be treated as false
   var inputCase = window.prompt("Does your password need lowercase AND uppercase letters? Please answer yes or no.");
   var inputCharacters = window.prompt("Does your password need to contain special characters? Please answer yes or no.");
 
@@ -28,15 +28,14 @@ function writePassword() {
 
   passwordText.value = password;
 
+  // Used backslashes rather than unicode to include all special characters
   function generatePassword() {
     if (caseSensitive && specialCharacters) {
       let password = ''
       const characters = "[`!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>\/?~]abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-      // need to figure out how to get / " and [] in there 
-      // figure out how to get this available
       const charactersLength = characters.length;
+
       // this is a for loop that uses passwordLength to determine i (how long it runs) and appends a new value chosen from the string for each appendage until the for loop is complete
-      
       for (let i = 0; i < passwordLength; i++) {
         password += characters.charAt(Math.floor(Math.random() * charactersLength));
       }
@@ -57,8 +56,6 @@ function writePassword() {
     else if (!caseSensitive && specialCharacters) {
       let password = ''
       const characters = "[`!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>\/?~]abcdefghijklmnopqrstuvwxyz1234567890"
-      // need to figure out how to get / " and [] in there 
-      // figure out how to get this available
       const charactersLength = characters.length;
 
       for (let i = 0; i < passwordLength; i++) {
@@ -71,7 +68,7 @@ function writePassword() {
       let password = ''
       const characters = "abcdefghijklmnopqrstuvwxyz1234567890";
       const charactersLength = characters.length;
-  
+
       for (let i = 0; i < passwordLength; i++) {
         password += characters.charAt(Math.floor(Math.random() * charactersLength));
       }
@@ -80,6 +77,5 @@ function writePassword() {
   }
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
