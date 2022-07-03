1de4a21a77122332fd73fdcc2ba262b85bbeaca3
aurumbright <aurum.elean@gmail.com>
2022-07-03 14:39:49 -0700

    redoing for loop

--- assets/script.js
+++ assets/script.js
@@ -17,63 +17,61 @@ function writePassword() {
   };
 
   // All input other than yes will be treated as false
-  var inputCase = window.prompt("Does your password need lowercase AND uppercase letters? Please answer yes or no.");
+  var inputUpperCase = window.prompt("Does your password need uppercase letters? Please answer yes or no.");
+  var inputLowerCase = window.prompt("Does your password need to contain lowercase letters? Please answer yes or no.")
   var inputCharacters = window.prompt("Does your password need to contain special characters? Please answer yes or no.");
+  var inputNumbers = window.prompt("Does your password need to include numbers? Please answer yes or no.")
 
-  var caseSensitive = (inputCase == "yes");
+  var lowerCase = (inputLowerCase == "yes");
+  var upperCase = (inputUpperCase == "yes");
   var specialCharacters = (inputCharacters == "yes");
+  var includeNumbers = (inputNumbers == "yes");
 
   var password = generatePassword(passwordLength);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-  // Used backslashes rather than unicode to include all special characters
-  function generatePassword() {
-    if (caseSensitive && specialCharacters) {
-      let password = ''
-      const characters = "[`!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>\/?~]abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-      const charactersLength = characters.length;
+  /* Separate arrays? 1 - 4 requirements
+  
+  Also DRYness of code - something can be simplified; strategy for identifying what needs to happen
+  2 criteria: length and diversity of pw
+  diversity criteria: up to 4
 
-      // this is a for loop that uses passwordLength to determine i (how long it runs) and appends a new value chosen from the string for each appendage until the for loop is complete
-      for (let i = 0; i < passwordLength; i++) {
-        password += characters.charAt(Math.floor(Math.random() * charactersLength));
+  */
+
+  function generatePassword(passwordLength, caseSensitive, specialCharacters, includeNumbers) {
+    var characterSetSpecial = "[`!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>\/?~]";
+    var characterSetCapitalized = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var characterSetLowercase = "abcdefghijklmnopqrstuvwxyz"
+    var characterSetNumbers = "1234567890"
+    var j = 0
+    var desiredCharacterArrays = [characterSetLowercase, characterSetSpecial, characterSetCapitalized, characterSetNumbers];
+    let password = ''
+
+    // this is a for loop that uses passwordLength to determine i (how long it runs) and appends a new value chosen from the string for each appendage until the for loop is complete
+    //replace if statement for all the loops:
+    for (let i = 0; i < passwordLength; i++) {
+
+      // pulling from arrays/strings based on the parameters of the if statement
+      // characters has to be defined - 
+      // how do we guarantee that we have, for as many diversity specs as we have, there is at least one of each? 
+      // rotate through each "array" - 1,2,3,4
+      // diversity requirement: 
+      // keep an index of which desiredCharacterArrays we're currently on
+      // beginning of for loop: make a variable for currentCharacterArray (j)
+
+      password += desiredCharacterArrays[j].charAt(Math.floor(Math.random() * charactersLength));
+
+      if (j < desiredCharacterArrays.length) {
+        j++;
+      } else {
+        j = 0
       }
-      return password;
     }
 
-    else if (caseSensitive && !specialCharacters) {
-      let password = ''
-      const characters = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      const charactersLength = characters.length;
+    return password;
 
-      for (let i = 0; i < passwordLength; i++) {
-        password += characters.charAt(Math.floor(Math.random() * charactersLength));
-      }
-      return password;
-    }
-
-    else if (!caseSensitive && specialCharacters) {
-      let password = ''
-      const characters = "[`!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>\/?~]abcdefghijklmnopqrstuvwxyz1234567890"
-      const charactersLength = characters.length;
-
-      for (let i = 0; i < passwordLength; i++) {
-        password += characters.charAt(Math.floor(Math.random() * charactersLength));
-      }
-      return password;
-    }
-
-    else if (!caseSensitive && !specialCharacters) {
-      let password = ''
-      const characters = "abcdefghijklmnopqrstuvwxyz1234567890";
-      const charactersLength = characters.length;
-
-      for (let i = 0; i < passwordLength; i++) {
-        password += characters.charAt(Math.floor(Math.random() * charactersLength));
-      }
-      return password;
-    };
   }
 }
 
